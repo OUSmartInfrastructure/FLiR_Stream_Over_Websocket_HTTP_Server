531159a9e05085cb0bc0ba57ebafc8f382bb574d
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2018-07-23 13:52:21 -0400

    Update websocket_http_server.py

--- websocket_http_server.py
+++ websocket_http_server.py
@@ -29,22 +29,16 @@ def program_start():
 	websocket_port = int(websocket_port)
 	image_path = parser.get('Server-Configurations', 'Image')
 	def capture(device = "/dev/spidev0.0"):
-		try:
-			with Lepton(device) as l:
-				a,_ = l.capture()
-			cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
-			np.right_shift(a, 8, a)
-			return np.uint8(a)
-		except:
-			pass
+		with Lepton(device) as l:
+			a,_ = l.capture()
+		cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
+		np.right_shift(a, 8, a)
+		return np.uint8(a)
 
 	def save_image():
 		while True:
-			try:
-				image = capture()
-				im = plt.imsave("thermal.jpg", image.mean(2),cmap="jet")
-			except:
-				pass
+			image = capture()
+			im = plt.imsave("thermal.jpg", image.mean(2),cmap="jet")
 	def http_server():
 		addr = socket.getaddrinfo(http_interface, http_port)[0][-1]
 		s = socket.socket()
@@ -53,17 +47,14 @@ def program_start():
 		s.listen(http_listen)
 		print('listening on', addr)
 		while True:
-			try:
-				c1, addr = s.accept()
-	        		#print repr(c1.recv(http_recieve))
-	        		#print('client connected from',addr)
-				data = open("index.html", 'rb')
-				data = data.read()
-	        		c1.send(data)
-				time.sleep(.01)
-	       			c1.close()
-			except:
-				pass
+			c1, addr = s.accept()
+	        	print repr(c1.recv(http_recieve))
+	        	print('client connected from',addr)
+			data = open("index.html", 'rb')
+			data = data.read()
+	        	c1.send(data)
+			time.sleep(.01)
+	       		c1.close()
 
 	def websocket_server():
 		global i
@@ -72,23 +63,26 @@ def program_start():
 			global i
 			i=((i+1))
 			def start_data_stream():
+				data_limit = 2000
 				while True:
+					check = os.stat('thermal.jpg')
 					#Add a wait in, due to file access speed
-					time.sleep(.01)
+					time.sleep(.005)
 					try:
-						with open(image_path, "rb") as image_file:
-							thermal=base64.b64encode(image_file.read())
-						server.send_message_to_all(thermal)
+						if check>=data_limit:
+							with open(image_path, "rb") as image_file:
+								thermal=base64.b64encode(image_file.read())
+							server.send_message_to_all(thermal)
+						else:
+							pass
 					except:
 						pass
-				else:
-					pass
 			if i == 1:
-				threading.Thread(target=start_data_stream).start()
+				start_data_stream()
 			else:
 				pass
 
-		        #print("New client connected and was given id %d" % client['id'])
+		        print("New client connected and was given id %d" % client['id'])
 
 		def client_left(client, server):
 		        print("Client(%d) disconnected" % client['id'])
